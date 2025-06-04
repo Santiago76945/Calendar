a1a238be6ee1a9d7fce3cdffb0d20821d71a3f99
Santiago76945 <santiago_76945@yahoo.es>
2025-06-03 21:03:03 -0300

    css changed and code issues fixed

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -6,18 +6,23 @@ export default function Home() {
   return (
     <div className="main-menu-card">
       <header className="header">
-        <h1 className="title">Mi App de Calendario</h1>
+        <h1 className="title">Kalendario</h1>
+        <h2>Una aplicación de Santiago Haspert</h2>
       </header>
 
       <div className="menu">
         <Link href="/calendario">
-          <div className="action">Ver Calendario</div>
+          <div className="action">Ver calendario</div>
         </Link>
 
         <Link href="/import-export">
-          <div className="action">Importar / Exportar Eventos</div>
+          <div className="action">Importar y exportar eventos</div>
         </Link>
       </div>
+
+      <h4>
+        Prototipo de uso personal. Su reproducción total o parcial está prohibida. © 2025 Santiago Haspert. Todos los derechos reservados.
+      </h4>
     </div>
   )
 }
